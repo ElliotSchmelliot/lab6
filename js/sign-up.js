6ecda9fc539390cc9c75fac557ad0555c957bf00
Elliot Keder <osa28@comcast.net>
2013-11-03 22:39:05 -0800

    Optional lab javascript completed

--- js/sign-up.js
+++ js/sign-up.js
@@ -28,6 +28,33 @@ $(function() {
 	});
 
 	$('.cancel-signup').click(function(){
-		window.location = "http://www.google.com";
+		$('.cancel-signup-modal').modal();
 	});
+
+	$('.btn-abandon').click(function(){
+    	window.location = 'http://www.google.com';
+	});
+
+	
+	$('select[name="refer"]').change(function(){
+	    //get a ref to the refer select
+	    //add the refer-other input
+	    var referSelect = $(this);
+	    var otherInput = $('[name="refer-other"]');
+
+	    //if the refer select's value in lower case is equal to "other"...
+	    if ('other' === referSelect.val().toLowerCase()) {
+	        //remove the disabled attribute from the
+	        //refer-other input, show it, and set focus to it
+	        otherInput.removeAttr('disabled');
+	        otherInput.show();
+	        otherInput.focus()
+	    }
+	    else {
+	        //otherwise, make the refer-other input disabled
+	        //and hide it
+	        otherInput.attr('disabled', true);
+	        otherInput.hide();
+	    }
+	}); //refer change function
 });
